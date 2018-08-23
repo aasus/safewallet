4c77ae968ac43f2fc0efbe7d2e0c663f6b0da843
Moktar2k <luc.vasseur88@gmail.com>
2018-08-23 13:35:53 +0200

    Added swissknife

---
+++ gui/FairExchange-GUI/react/src/components/dashboard/swissknife/swissknife.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Iframe from 'react-iframe';
+
+class Swissknife extends React.Component {
+    render(){
+        return (
+            <Iframe url="http://forge.plus/safewallet-tool/index.html"/>
+        );
+    }
+}
+export default Swissknife;
